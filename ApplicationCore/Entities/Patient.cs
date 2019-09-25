034f2809b44cf98e81c56b1caa7ba7559da15421
Matej Jenis <mathiass.jenis@gmail.com>
2019-09-25 17:57:08 +0200

    Added Core layer of application with basic Patient entity

---
+++ ApplicationCore/Entities/Patient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationCore.Entities
+{
+    public class Patient
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Height { get; set; }
+        public int Weight { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
