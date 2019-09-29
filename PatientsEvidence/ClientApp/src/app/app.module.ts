ff8ca4b254319507983de0fde8171077758e224c
Matej Jenis <mathiass.jenis@gmail.com>
2019-09-29 12:13:35 +0200

    Code cleanup

--- PatientsEvidence/ClientApp/src/app/app.module.ts
+++ PatientsEvidence/ClientApp/src/app/app.module.ts
@@ -5,20 +5,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { PatientsComponent } from './patients/patients.component';
 import { PatientDetailComponent } from './patient-detail/patient-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent,
-    HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
     PatientsComponent,
     PatientDetailComponent
   ],
@@ -27,9 +19,7 @@ import { PatientDetailComponent } from './patient-detail/patient-detail.componen
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
+      { path: '', component: PatientsComponent, pathMatch: 'full' },
       { path: 'patients', component: PatientsComponent },
       { path: 'patient-detail/:id', component: PatientDetailComponent }
     ])
